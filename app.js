834d3ccd15c2a7781f6acf2b0eb2e6e4b5721f05
NathanSt10 <nstout@uwm.edu>
2024-03-31 12:44:40 -0500

    Implemented color change on mouseover event on a grid element

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ addEventListener("mouseover", (event) => {});
 
 // let userResponse = prompt();
 
-for (let i = 4; i > 0; --i) {
+for (let i = 64; i > 0; --i) {
     const i = document.createElement("div")
     i.classList.add("row")
     divBody.appendChild(i);
-    for (let j = 4; j > 0; --j) {
+    for (let j = 64; j > 0; --j) {
         const j = document.createElement("div");
         j.classList.add("column");
-        j.addEventListener("mouseover", () => {ChangeColor();})
+        j.addEventListener("mouseover", () => {ChangeColor(j);})
         i.appendChild(j);
     }
 }
 
-let ChangeColor = function() {
-    console.log("hello");
+let ChangeColor = function(grid) {
+    grid.style.backgroundColor = "black";
 }
