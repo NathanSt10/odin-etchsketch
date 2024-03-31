ded59356c4d93cda0c970f9473b9543c62173ac8
NathanSt10 <nstout@uwm.edu>
2024-03-30 21:46:24 -0500

    added mouse over event listener to the grid

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const divBody = document.querySelector("#divBody");
 
+addEventListener("mouseover", (event) => {});
+
 // let userResponse = prompt();
 
 for (let i = 4; i > 0; --i) {
     const i = document.createElement("div")
-    i.classList.add("column")
+    i.classList.add("row")
     divBody.appendChild(i);
     for (let j = 4; j > 0; --j) {
         const j = document.createElement("div");
-        j.classList.add("row");
-        j.textContent = 1;
+        j.classList.add("column");
+        j.addEventListener("mouseover", () => {ChangeColor();})
         i.appendChild(j);
     }
+}
+
+let ChangeColor = function() {
+    console.log("hello");
 }
