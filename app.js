10553a72c24e4636ff042967839ffff064626cb6
NathanSt10 <nstout@uwm.edu>
2024-03-30 21:32:35 -0500

    Used javascript to create elements for a grid and used flexbox to position them

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const body = document.querySelector("body");
-const divBody = document.createElement("div");
-body.appendChild(divBody);
+const divBody = document.querySelector("#divBody");
 
-let userResponse = prompt();
+// let userResponse = prompt();
+
+for (let i = 4; i > 0; --i) {
+    const i = document.createElement("div")
+    i.classList.add("column")
+    divBody.appendChild(i);
+    for (let j = 4; j > 0; --j) {
+        const j = document.createElement("div");
+        j.classList.add("row");
+        j.textContent = 1;
+        i.appendChild(j);
+    }
+}
