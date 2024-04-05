6badaef12de89ce0789d30d91abf38779405ec21
NathanSt10 <nstout@uwm.edu>
2024-04-04 21:50:00 -0500

    Fixed bug where grid size 1-3 would not work. App now supports changing grid size.

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
+window.onload = function() {
+    buildGrid(16)
+}; 
+
+const sizeButton = document.querySelector("#sizeButton")
+sizeButton.addEventListener("click", () => {changeSize();})
+
 const divBody = document.querySelector("#divBody");
-const gridContainer = document.createElement("div");
-gridContainer.classList.add("container");
-divBody.appendChild(gridContainer);
 
-<<<<<<< HEAD
-addEventListener("mouseover", (event) => {});
-
-// let userResponse = prompt();
-
-for (let i = 8; i > 0; --i) {
-    const i = document.createElement("div")
-    i.classList.add("row")
-    divBody.appendChild(i);
-    for (let j = 8; j > 0; --j) {
-=======
-let size = 8;
-for (let i = size; i > 0; --i) {
-    const i = document.createElement("div")
-    i.classList.add("row")
-    gridContainer.appendChild(i);
-    for (let j = size; j > 0; --j) {
->>>>>>> etchsketch-lockgrid
-        const j = document.createElement("div");
-        j.classList.add("column");
-        j.addEventListener("mouseover", () => {ChangeColor(j);})
-        i.appendChild(j);
-    }
+let changeSize = function() {
+    size = prompt();
+    deleteGrid(size);
+    buildGrid(size);
 }
 
-let ChangeColor = function(grid) {
+let buildGrid = function(size) {
+    const gridContainer = document.createElement("div");
+    gridContainer.classList.add("container");
+    for (let i = size; i > 0; --i) {
+        const i = document.createElement("div")
+        i.classList.add("column")
+        gridContainer.appendChild(i);
+        for (let j = size; j > 0; --j) {
+            const j = document.createElement("div");
+            j.classList.add("row");
+            j.addEventListener("mouseover", () => {changeColor(j);})
+            i.appendChild(j);
+        }
+    }    
+    divBody.appendChild(gridContainer);
+}
+
+let deleteGrid = function(size) {
+    const gridContainer = document.querySelector(".container");
+    divBody.removeChild(gridContainer);
+}
+
+let changeColor = function(grid) {
     grid.style.backgroundColor = "black";
 }
