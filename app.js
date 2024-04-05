72444e2a186f845f2715a0eb26021a18e640d54f
NathanSt10 <nstout@uwm.edu>
2024-04-04 19:59:43 -0500

    update size

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ addEventListener("mouseover", (event) => {});
 
 // let userResponse = prompt();
 
-for (let i = 64; i > 0; --i) {
+for (let i = 8; i > 0; --i) {
     const i = document.createElement("div")
     i.classList.add("row")
     divBody.appendChild(i);
-    for (let j = 64; j > 0; --j) {
+    for (let j = 8; j > 0; --j) {
         const j = document.createElement("div");
         j.classList.add("column");
         j.addEventListener("mouseover", () => {ChangeColor(j);})
