eff946cfd5d96d30b42471bda8ffea1a2ed891ee
NathanSt10 <nstout@uwm.edu>
2024-04-04 19:56:46 -0500

    update

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const divBody = document.querySelector("#divBody");
+const gridContainer = document.createElement("div");
+gridContainer.classList.add("container");
+divBody.appendChild(gridContainer);
 
 let size = 4;
 for (let i = size; i > 0; --i) {
     const i = document.createElement("div")
     i.classList.add("row")
-    divBody.appendChild(i);
+    gridContainer.appendChild(i);
     for (let j = size; j > 0; --j) {
         const j = document.createElement("div");
         j.classList.add("column");
