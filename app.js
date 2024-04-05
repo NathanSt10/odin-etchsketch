c5393fbda04f2100d1d5f53b5aa8e76e32e0ac2a
NathanSt10 <nstout@uwm.edu>
2024-04-04 19:35:36 -0500

    init commit

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const divBody = document.querySelector("#divBody");
 
-addEventListener("mouseover", (event) => {});
-
-// let userResponse = prompt();
-
-for (let i = 64; i > 0; --i) {
+let size = 4;
+for (let i = size; i > 0; --i) {
     const i = document.createElement("div")
     i.classList.add("row")
     divBody.appendChild(i);
-    for (let j = 64; j > 0; --j) {
+    for (let j = size; j > 0; --j) {
         const j = document.createElement("div");
         j.classList.add("column");
         j.addEventListener("mouseover", () => {ChangeColor(j);})
